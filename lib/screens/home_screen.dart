acb093d0edb095c15d7068efaf59039d82c11825
4uRest <4urest@192.168.1.55>
2024-03-31 10:37:08 -0600

    MVP11

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -361,3 +361,369 @@ class _ProductItem extends StatelessWidget {
             }));
   }
 }
+
+
+/*import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:menu/bloc/scroll_tabbar_bloc.dart';
+import 'package:menu/models/branch_catalog_response.dart';
+import 'package:menu/providers/branch_catalog_provider.dart';
+import 'package:menu/screens/shared/items_widget.dart';
+import 'package:menu/screens/single_item_screen.dart';
+import 'package:menu/screens/welcome_screen.dart';
+import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+const _backgroundColor = Color(0xFFF6F9FA);
+const _blueColor = Color(0xFF0D1863);
+const _greenColor = Color(0xFF2BBEBA);
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
+  final _bloc = ScrollTabBarBLoC();
+
+  void _launchUrl(Uri url) async {
+    try {
+      final bool launched = await launchUrl(url);
+      if (!launched) {}
+    } catch (e) {}
+  }
+
+  TabController? _catalogTabController;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    final branchCatalogProvider = Provider.of<BranchCatalogProvider>(context);
+    final catalogs = branchCatalogProvider.branchCatalog?.catalogs ?? [];
+
+    _catalogTabController?.dispose();
+    if (catalogs.isNotEmpty) {
+      _catalogTabController =
+          TabController(length: catalogs.length, vsync: this);
+
+      _bloc.setCatalogId(catalogs.first.id, this, context);
+      _catalogTabController!.animateTo(0);
+
+      _catalogTabController!.addListener(() {
+        if (!_catalogTabController!.indexIsChanging) {
+          final selectedCatalogId = catalogs[_catalogTabController!.index].id;
+          _bloc.setCatalogId(selectedCatalogId, this, context);
+        }
+      });
+    } else {
+      print('No catalogs found, TabController not initialized.');
+    }
+  }
+
+  @override
+  void dispose() {
+    _bloc.dispose();
+    _catalogTabController?.dispose();
+    _bloc.categoryTabController?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final branchCatalogProvider = Provider.of<BranchCatalogProvider>(context);
+    final backgroundImage =
+        branchCatalogProvider.branchCatalog?.menuBackground ?? '';
+    final catalogs = branchCatalogProvider.branchCatalog?.catalogs ?? [];
+    final brandName =
+        branchCatalogProvider.branchCatalog?.brandName ?? 'Empresa';
+    final branchName =
+        branchCatalogProvider.branchCatalog?.branchName ?? 'Sucursal';
+    return ChangeNotifierProvider.value(
+      value: _bloc,
+      child: Scaffold(
+        body: Column(
+          children: [
+            Expanded(
+              child: Material(
+                child: Container(
+                  color: Colors.black,
+                  child: Center(
+                    child: SizedBox(
+                      width: 520,
+                      height: 932,
+                      child: Container(
+                        padding: EdgeInsets.only(top: 2, bottom: 20),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          image: DecorationImage(
+                            image: backgroundImage.isNotEmpty
+                                ? NetworkImage(backgroundImage)
+                                    as ImageProvider<Object>
+                                : AssetImage(
+                                        'images/tools/FruteriaLaUnica_bg.jpg')
+                                    as ImageProvider<Object>,
+                            fit: BoxFit.cover,
+                            opacity: 1,
+                          ),
+                        ),
+                        child: Column(
+                          children: [
+                            Column(
+                              children: [
+                                Center(
+                                  // Centro el texto de la marca
+                                  child: Text(
+                                    brandName,
+                                    textAlign: TextAlign.center,
+                                    style: GoogleFonts.lato(
+                                      fontSize: 28,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                                Center(
+                                  // Centro el texto de la marca
+                                  child: Text(
+                                    branchName, // Usa la variable brandName aquí
+                                    textAlign: TextAlign
+                                        .center, // Alineo el texto al centro
+                                    style: GoogleFonts.pacifico(
+                                        fontSize: 24, color: Colors.white),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            TabBar(
+                              isScrollable: true,
+                              controller: _catalogTabController,
+                              labelStyle: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 14,
+                              ),
+                              labelColor: Colors.white,
+                              unselectedLabelColor:
+                                  Colors.white.withOpacity(0.5),
+                              tabs: catalogs
+                                  .map((catalog) => Tab(text: catalog.name))
+                                  .toList(),
+                            ),
+                            Expanded(
+                              child: Container(
+                                child: Column(
+                                  crossAxisAlignment:
+                                      CrossAxisAlignment.stretch,
+                                  children: [
+                                    Container(
+                                      //color: _backgroundColor,
+                                      height: 60,
+                                      child: Consumer<ScrollTabBarBLoC>(
+                                        builder: (context, bloc, child) {
+                                          if (bloc.categoryTabController ==
+                                              null) {
+                                            print(
+                                                'categoryTabController es nulo');
+                                            return Center(
+                                                child:
+                                                    CircularProgressIndicator());
+                                          } else {
+                                            return AnimatedBuilder(
+                                              animation:
+                                                  bloc.categoryTabController!,
+                                              builder: (context, child) {
+                                                assert(bloc
+                                                        .categoryTabController!
+                                                        .length ==
+                                                    bloc.tabs.length);
+                                                return TabBar(
+                                                  onTap:
+                                                      _bloc.onCategorySelected,
+                                                  controller: bloc
+                                                      .categoryTabController,
+                                                  isScrollable: true,
+                                                  tabs: bloc.tabs
+                                                      .map((e) => _TabWidget(e))
+                                                      .toList(),
+                                                );
+                                              },
+                                            );
+                                          }
+                                        },
+                                      ),
+                                    ),
+                                    Expanded(
+                                      child: Container(
+                                        //color: _backgroundColor,
+                                        child: Consumer<ScrollTabBarBLoC>(
+                                          builder: (context, bloc, child) {
+                                            return ListView.builder(
+                                              controller: bloc.scrollController,
+                                              itemCount: bloc.items.length,
+                                              padding:
+                                                  const EdgeInsets.symmetric(
+                                                      horizontal: 20),
+                                              itemBuilder: (context, index) {
+                                                final item = bloc.items[index];
+                                                if (item.isCategory) {
+                                                  return _CategoryItem(
+                                                      item.category!);
+                                                } else {
+                                                  if (item.item != null) {
+                                                    return _ProductItem(
+                                                        item.item!);
+                                                  } else {
+                                                    return Container();
+                                                  }
+                                                }
+                                              },
+                                            );
+                                          },
+                                        ),
+                                      ),
+                                    ),
+                                    Container(
+                                      color: Colors.black,
+                                      padding:
+                                          EdgeInsets.symmetric(vertical: 10),
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          GestureDetector(
+                                            onTap: () {
+                                              _launchUrl(Uri.parse(
+                                                  'https://www.4urest.mx'));
+                                            },
+                                            child: Text(
+                                              'Diseñado por ',
+                                              style: TextStyle(
+                                                color: Colors.white
+                                                    .withOpacity(0.8),
+                                                fontSize: 16,
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                          ),
+                                          SizedBox(height: 5),
+                                          GestureDetector(
+                                            onTap: () {
+                                              _launchUrl(Uri.parse(
+                                                  'https://www.4urest.mx'));
+                                            },
+                                            child: Image.asset(
+                                              'images/tools/4uRestFont-white.png',
+                                              height:
+                                                  35, // Ajusta esto según sea necesario para que coincida con el diseño deseado
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => WelcomeScreen()),
+            );
+          },
+          child: Icon(
+            Icons.home,
+            color: Colors.white,
+          ),
+          backgroundColor: Color(0xFFE57734),
+        ),
+      ),
+    );
+  }
+}
+
+class _TabWidget extends StatelessWidget {
+  const _TabWidget(this.categoryTab);
+
+  final CategoryTab categoryTab;
+
+  @override
+  Widget build(BuildContext context) {
+    final selected = categoryTab.selected;
+    return Opacity(
+      opacity: selected ? 1 : 0.5,
+      child: Card(
+        elevation: selected ? 6 : 0,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            categoryTab.category.name,
+            style: TextStyle(
+              color: _blueColor,
+              fontWeight: FontWeight.bold,
+              fontSize: 13,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _CategoryItem extends StatelessWidget {
+  const _CategoryItem(this.category);
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Container(
+        height: categoryHeight,
+        alignment: Alignment.centerLeft,
+        child: Text(
+          category.name,
+          style: GoogleFonts.pacifico(fontSize: 24, color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
+
+class _ProductItem extends StatelessWidget {
+  const _ProductItem(this.product);
+  final Item product;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: productHeight,
+        alignment: Alignment.centerLeft,
+        child: MenuItemCard(
+            cardType: "product",
+            item: product,
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => SingleItemScreen(item: product),
+                ),
+              );
+            }));
+  }
+}
+*/
--- lib/screens/shared/items_widget.dart
+++ lib/screens/shared/items_widget.dart
@@ -49,6 +49,270 @@ class MenuItemCard extends StatelessWidget {
               text: TextSpan(
                 children: [
                   TextSpan(
+                    text: adjustedText.substring(0, dashPosition + 1),
+                    style: priceStyle,
+                  ),
+                  TextSpan(
+                      text: adjustedText.substring(dashPosition + 1),
+                      style: TextStyle(color: Colors.black)),
+                ],
+              ),
+            ),
+            usesTwoLines);
+      } else {
+        return Tuple2(
+            Text(adjustedText,
+                style: TextStyle(color: Colors.black),
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis),
+            usesTwoLines);
+      }
+    }
+
+    if (shouldShowPrice || item.description.isNotEmpty) {
+      String realDescription = item.description;
+      bool descriptionRequiresTwoLines = realDescription.length >= minLimit;
+      bool combinedTextRequiresTwoLines = (shouldShowPrice &&
+          (item.price.length + realDescription.length >= minLimit));
+      bool usesTwoLines =
+          descriptionRequiresTwoLines || combinedTextRequiresTwoLines;
+
+      Tuple2<Widget, bool> descriptionAndPriceResult =
+          buildTextWithLimit(realDescription, item.price, shouldShowPrice);
+      Widget descriptionAndPriceWidget = descriptionAndPriceResult.item1;
+      widgetsCount += usesTwoLines ? 2 : 1;
+      contentWidgets.add(descriptionAndPriceWidget);
+    }
+
+    if (contentWidgets.length == maxWidgetsToShow - 1) {
+      contentWidgets.add(
+        GestureDetector(
+          onTap: onTap,
+          child: Row(
+            children: [
+              Container(
+                width: 100,
+                child: Icon(
+                  Icons.more_vert,
+                  color: Colors.black,
+                  size: 20,
+                ),
+              ),
+              Text(
+                "Ver más",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15),
+              ),
+              Icon(
+                Icons.arrow_forward_ios,
+                color: Colors.black.withOpacity(0.5),
+                size: 20,
+              ),
+            ],
+          ),
+        ),
+      );
+    } else {
+      List<Widget> modifierWidgets = buildModifiersWidgets();
+      if ((contentWidgets.length + modifierWidgets.length) <=
+          maxWidgetsToShow - 1) {
+        contentWidgets.addAll(modifierWidgets);
+      } else {
+        List<Widget> modifierWidgets = buildModifiersWidgets();
+        if ((contentWidgets.length + modifierWidgets.length) <=
+            maxWidgetsToShow - 1) {
+          contentWidgets.addAll(modifierWidgets);
+        } else {
+          int remainingSpace = maxWidgetsToShow - 1 - contentWidgets.length;
+          contentWidgets.addAll(modifierWidgets.take(remainingSpace));
+          contentWidgets.add(
+            GestureDetector(
+              onTap: onTap,
+              child: Row(
+                children: [
+                  Container(
+                    width: 100,
+                    child: Icon(
+                      Icons.more_vert,
+                      color: Colors.black.withOpacity(0.5),
+                      size: 20,
+                    ),
+                  ),
+                  Text(
+                    "Ver más",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 12),
+                  ),
+                  Icon(
+                    Icons.arrow_forward_ios,
+                    color: Colors.black,
+                    size: 15,
+                  ),
+                  // icono aqui
+                ],
+              ),
+            ),
+          );
+        }
+      }
+    }
+
+    String buildModifiersString() {
+      List<String> modifiersList = [];
+
+      for (var group in item.modifiersGroups) {
+        for (var modifier in group.modifiers) {
+          final bool shouldShowModifierPrice = group.isSelectable != 'True' &&
+              modifier.price.isNotEmpty &&
+              modifier.price != '0' &&
+              modifier.price != '0.00';
+
+          String priceText = shouldShowModifierPrice
+              ? ' - \$${double.parse(modifier.price).toStringAsFixed(2)}'
+              : '';
+
+          String modifierText = '${modifier.alias}$priceText';
+          modifiersList.add(modifierText);
+        }
+      }
+
+      String modifiersString = modifiersList.join(', ');
+      return modifiersString;
+    }
+
+    String modifiersString = buildModifiersString();
+    print(modifiersString);
+
+    return Card(
+      color: Colors.white.withOpacity(0.85),
+      elevation: 4.0,
+      margin: const EdgeInsets.symmetric(horizontal: 2, vertical: 4),
+      child: InkWell(
+        onTap: onTap,
+        child: Padding(
+          padding: const EdgeInsets.all(6.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: contentWidgets,
+                ),
+              ),
+              if (item.icon != null && item.icon!.isNotEmpty)
+                Padding(
+                  padding: const EdgeInsets.only(left: 16),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    child: Image.network(
+                      item.icon!,
+                      width: 100,
+                      height: 100,
+                      fit: BoxFit.cover,
+                      errorBuilder: (context, error, stackTrace) =>
+                          Container(width: 0, height: 0),
+                    ),
+                  ),
+                ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> buildModifiersWidgets() {
+    List<Widget> widgetsList = [];
+
+    for (var group in item.modifiersGroups) {
+      List<Widget> modifierWidgets = [];
+      for (var modifier in group.modifiers) {
+        final bool shouldShowModifierPrice = group.isSelectable != 'True' &&
+            modifier.price.isNotEmpty &&
+            modifier.price != '0' &&
+            modifier.price != '0.00';
+        String priceText = shouldShowModifierPrice
+            ? ' - \$${double.parse(modifier.price).toStringAsFixed(2)}'
+            : '';
+        Widget modifierTextWidget = Row(
+          children: [
+            Text('• ${modifier.alias}'),
+            if (shouldShowModifierPrice) buildPriceText(priceText, true),
+          ],
+        );
+        modifierWidgets.add(modifierTextWidget);
+      }
+
+      if (modifierWidgets.isNotEmpty) {
+        widgetsList.add(Text(group.alias,
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12)));
+        widgetsList.addAll(modifierWidgets);
+      }
+    }
+    return widgetsList;
+  }
+
+  Widget buildPriceText(String text, bool isPrice) {
+    return Text(text, style: isPrice ? priceStyle : null);
+  }
+}
+
+/*import 'package:flutter/material.dart';
+import 'package:menu/models/branch_catalog_response.dart';
+import 'package:tuple/tuple.dart';
+
+class MenuItemCard extends StatelessWidget {
+  final String cardType;
+  final Item item;
+  final VoidCallback onTap;
+  final int maxWidgetsToShow = 5;
+  final int maxLimit = 50;
+  final int minLimit = 25;
+  final TextStyle priceStyle = TextStyle(
+    color: Colors.green,
+    fontStyle: FontStyle.italic,
+    fontWeight: FontWeight.bold,
+  );
+
+  MenuItemCard({
+    Key? key,
+    required this.cardType,
+    required this.item,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final bool shouldShowPrice =
+        item.price.isNotEmpty && item.price != '0' && item.price != '0.00';
+
+    List<Widget> contentWidgets = [
+      Text(item.alias,
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+    ];
+    int widgetsCount = 1;
+
+    Tuple2<Widget, bool> buildTextWithLimit(
+        String description, String price, bool isPrice) {
+      bool usesTwoLines = description.length >= minLimit;
+      String fullText = isPrice ? '\$$price - $description' : description;
+      String adjustedText = fullText.length > maxLimit
+          ? '${fullText.substring(0, maxLimit)}...'
+          : fullText;
+      if (isPrice) {
+        int dashPosition = adjustedText.indexOf(' - ');
+        return Tuple2(
+            RichText(
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              text: TextSpan(
+                children: [
+                  TextSpan(
                       text: adjustedText.substring(0, dashPosition + 1),
                       style: TextStyle(color: Colors.green)),
                   TextSpan(
@@ -147,6 +411,32 @@ class MenuItemCard extends StatelessWidget {
       }
     }
 
+    String buildModifiersString() {
+      List<String> modifiersList = [];
+
+      for (var group in item.modifiersGroups) {
+        for (var modifier in group.modifiers) {
+          final bool shouldShowModifierPrice = group.isSelectable != 'True' &&
+              modifier.price.isNotEmpty &&
+              modifier.price != '0' &&
+              modifier.price != '0.00';
+
+          String priceText = shouldShowModifierPrice
+              ? ' - \$${double.parse(modifier.price).toStringAsFixed(2)}'
+              : '';
+
+          String modifierText = '${modifier.alias}$priceText';
+          modifiersList.add(modifierText);
+        }
+      }
+
+      String modifiersString = modifiersList.join(', ');
+      return modifiersString;
+    }
+
+    String modifiersString = buildModifiersString();
+    print(modifiersString);
+
     return Card(
       color: Colors.white.withOpacity(0.85),
       elevation: 4.0,
@@ -221,3 +511,4 @@ class MenuItemCard extends StatelessWidget {
     return Text(text, style: isPrice ? priceStyle : null);
   }
 }
+*/
